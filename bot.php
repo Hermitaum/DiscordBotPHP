2de6702666db6708b5504a90ecb2d93a90a414ce
Hermitaum <48372648+Hermitaum@users.noreply.github.com>
2019-04-14 16:59:15 -0300

    Create bot.php

---
+++ bot.php
@@ -0,0 +1,41 @@
+<?php
+include "discordGateway.php";
+include "discordBot.php";
+
+use Discord\Gateway;
+use Discord\DiscordBOT;
+
+$discord = new DiscordBOT([
+	"token" => "BOT_TOKEN",
+	"bot" => true
+]);
+
+//Discord Bot written in PHP
+//Created by Hermit
+
+$discord->start(function() use ($discord){
+	echo "BOT INICIADO!", PHP_EOL;
+
+	$discord->on('message', function($message){
+		echo "{$message->author->username}: {$message->content}", PHP_EOL;
+
+		switch ($message->content) {
+			case "!privado":
+				$message->channel->replyMessage("Ola, aqui estou eu no privado!");
+			break;
+			case "!grupo":
+				$message->channel->sendMessage("Ola, aqui estou eu no grupo!");
+			break;
+			case "!embed":
+				$message->channel->sendMessage("", ["title" => "Ola!", "description" => "Aqui estou eu na forma embed!"]);
+			break;
+		}
+
+	});
+
+	$discord->on('messageDelete', function($message){
+		echo "UM USUARIO APAGOU A MENSAGEM", PHP_EOL;
+	});
+
+});
+?>
